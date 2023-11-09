1c84c99a8996fe914be6785c7ef46addf7cec797
noobwang233 <1213236870@qq.com>
2023-11-09 19:21:38 +0800

    temp save

--- Drivers/testkey.c
+++ Drivers/testkey.c
@@ -1,5 +1,5 @@
-#include <linux/types.h>   // 定义了ssize_t的头文件
 #include <linux/kernel.h>
+#include <linux/types.h>   // 定义了ssize_t的头文件
 #include <linux/ide.h>
 #include <linux/init.h>    // 模块加载init和卸载exit相关头文件
 #include <linux/module.h>
@@ -12,49 +12,83 @@
 #include <linux/miscdevice.h> //miscdevice头文件
 #include <linux/platform_device.h> //platform_device头文件
 #include <linux/of_platform.h> //platform of函数
+#include <asm/string.h>
+#include <linux/slab.h> //kmalloc头文件
+#include <linux/string.h>
 
-
-#define KEY_MISC_MINOR  234        /* KEY_MISC的次设备号 */
-#define KEY_MISC_NAME  "key_misc"  /* 驱动的主设备名称 */
-#define DEVICE_CNT    1            /* 设备数量 */
-
+#define KEY_MAJOR 234
+/*private date*/
+static u8 key_dev_count = 0; /* 设备计数 */
+static struct class *key_cls;//设备类
+/* device struct 设备结构体*/
 struct key_dev_t
 {
-    struct device_node *np;
+    unsigned int minor;
+    struct platform_device *key_pdev;
     int gpio; /* key 所使用的 GPIO 编号 */
-    struct miscdevice *misc;
-    spinlock_t lock;
-    struct platform_driver *p_driver;
+    spinlock_t lock; /*设备互斥访问自旋锁*/
     bool status; /*设备状态*/
     bool value; /*按键状态*/
+    struct cdev *key_cdev; /*字符设备结构体*/
+    struct class *cls;
+    struct device *dev;
 };
-static struct file_operations key_drv_fop;
-struct platform_driver key_platform_driver;
-/* MISC 设备结构体 */    /* 填充miscdevice结构体*/
-static struct miscdevice key_misc_dev = {
-    .minor = KEY_MISC_MINOR,
-    .name = KEY_MISC_NAME,
-    .fops = &key_drv_fop,
+// match table
+const struct of_device_id keys_of_match_table[] = {
+    {.compatible = "key_gpio",},
+    {},
 };
-struct key_dev_t key_dev_0 = {
-    .misc = &key_misc_dev,
-    .p_driver = &key_platform_driver,
+
+/* private function declear */
+// file_operation functions
+static int key_drv_open(struct inode *inode, struct file *filp);
+static int key_drv_release(struct inode *inode, struct file *filp);
+static ssize_t key_drv_read(struct file *filp, char __user *buf, size_t cnt, loff_t *offt);
+static ssize_t key_drv_write(struct file *filp, const char __user *buf, size_t cnt, loff_t *offt);
+static int key_drv_probe(struct platform_device *device);
+static int key_drv_remove(struct platform_device *device);
+//module init and exit
+static int __init key_drv_init(void);
+static void __exit key_drv_exit(void);
+//private key init and deinit function
+static int key_dev_init(struct key_dev_t *key_dev);
+static int key_drv_deinit(struct key_dev_t *key_dev);
+
+/* 驱动提供的文件操作结构体 */
+static struct file_operations key_drv_fop = {
+    .owner = THIS_MODULE,
+    .open = key_drv_open,
+    .release = key_drv_release,
+    .write = key_drv_write,
+    .read = key_drv_read,
 };
+/* 用于注册平台驱动的platform_driver结构体 */
+static struct platform_driver key_platform_driver = {
+    .probe = key_drv_probe,
+    .remove = key_drv_remove,
+
+    .driver = {
+        .name = "key_gpio",
+        .of_match_table = keys_of_match_table,
+    },
+};
+
 
 static int key_drv_open(struct inode *inode, struct file *filp)
 {
     unsigned long flags;/*中断标记*/
 
-    filp->private_data = &key_dev_0;
-    spin_lock_irqsave(&key_dev_0.lock, flags);//上锁
-    if(key_dev_0.status != true) //设备忙
+    struct key_dev_t *key_dev = container_of(&(inode->i_cdev), struct key_dev_t, key_cdev);//获取当前打开设备文件对应的设备结构体变量指针
+    filp->private_data = key_dev;
+    spin_lock_irqsave(&(key_dev->lock), flags);//上锁
+    if(key_dev->status != true) //设备忙
     {
-        spin_unlock_irqrestore(&key_dev_0.lock, flags);//释放锁
+        spin_unlock_irqrestore(&(key_dev->lock), flags);//释放锁
         pr_err("key_drv busy!\n");
         return -EBUSY;
     }
-    key_dev_0.status = false;//占用设备
-    spin_unlock_irqrestore(&key_dev_0.lock, flags);//释放锁
+    key_dev->status = false;//占用设备
+    spin_unlock_irqrestore(&(key_dev->lock), flags);//释放锁
     printk("key_drv open!\r\n");
     return 0;
 }
@@ -62,10 +96,11 @@ static int key_drv_open(struct inode *inode, struct file *filp)
 static int key_drv_release(struct inode *inode, struct file *filp)
 {
     unsigned long flags;/*中断标记*/
-
-    spin_lock_irqsave(&key_dev_0.lock, flags);//上锁
-    key_dev_0.status = true;//释放设备
-    spin_unlock_irqrestore(&key_dev_0.lock, flags);//释放锁
+    
+    struct key_dev_t *key_dev = filp->private_data;
+    spin_lock_irqsave(&(key_dev->lock), flags);//上锁
+    key_dev->status = true;//释放设备
+    spin_unlock_irqrestore(&(key_dev->lock), flags);//释放锁
     printk("key_drv close!\r\n");
     return 0;
 }
@@ -75,7 +110,8 @@ static ssize_t key_drv_read(struct file *filp, char __user *buf, size_t cnt, lof
     int retvalue = 0;
     u8 status = 0;
 
-    status = gpio_get_value(key_dev_0.gpio);
+    struct key_dev_t *key_dev = filp->private_data;
+    status = gpio_get_value(key_dev->gpio);
     /* 向用户空间发送数据 */
     retvalue = copy_to_user(buf, &status, cnt);
     if(retvalue == 0)
@@ -97,6 +133,7 @@ static ssize_t key_drv_write(struct file *filp, const char __user *buf, size_t c
     int retvalue = 0;
     u8 cmd = 0;
 
+    struct key_dev_t *key_dev = filp->private_data;
     retvalue = copy_from_user(&cmd, buf, cnt);
     if(retvalue == 0)
     {
@@ -111,18 +148,124 @@ static ssize_t key_drv_write(struct file *filp, const char __user *buf, size_t c
     switch (cmd)
     {
     case 0:// GPIO_ACTIVE_LOW
-        gpio_set_value(key_dev_0.gpio, 0);
+        gpio_set_value(key_dev->gpio, 0);
         printk("key on\n");
         break;
     case 1:
-        gpio_set_value(key_dev_0.gpio, 1);
+        gpio_set_value(key_dev->gpio, 1);
         printk("key off\n");
         break;
     default:
-        gpio_set_value(key_dev_0.gpio, (gpio_get_value(key_dev_0.gpio)==0? 1:0));
+        gpio_set_value(key_dev->gpio, (gpio_get_value(key_dev->gpio)==0? 1:0));
         printk("key trigger\n");
         break;
     }
+    return 0;
+}
+/*
+key_dev: 按键设备的结构体指针
+*/
+static int key_dev_init(struct key_dev_t *key_dev)
+{
+    int retvalue;
+
+    struct device_node *np = key_dev->key_pdev->dev.of_node;
+
+    /* 获取gpio号 */
+    key_dev->gpio = of_get_named_gpio(np, "gpios", 0);
+    if (key_dev->gpio <= 0)
+    {
+        pr_err("get gpio failed\n");
+        return -EIO;
+    }
+    printk("get gpio %d successfully! \n", key_dev->gpio);
+
+    /* 申请gpio */
+    retvalue = gpio_request(key_dev->gpio, "key_gpio");
+    if (retvalue != 0)
+    {
+        pr_err("request gpio failed\n");
+        return -EIO;
+    }
+    printk("request gpio %d successfully! \n", key_dev->gpio);
+    /* 设置为输入 */
+    retvalue = gpio_direction_input(key_dev->gpio);
+    if (retvalue != 0)
+    {
+        pr_err("set gpio %d input failed! \n", key_dev->gpio);
+        goto freegpio;
+    }
+    printk("set gpio %d input successfully! \n", key_dev->gpio);
+
+
+    
+    /* 使用cdev注册字符设备 */
+    key_dev->key_cdev = cdev_alloc();//申请cdev字符设备的空间
+    if(key_dev->key_cdev == NULL )
+    {
+        pr_err("key_dev key_cdev kmalloc failed! \n");
+        goto freegpio;
+    }
+    printk("key_dev key_cdev kmalloc successfully!\n");
+
+    /*生成设备号*/
+    #ifdef KEY_MAJOR
+        key_dev->key_cdev->dev = MKDEV(KEY_MAJOR, key_dev_count);
+        retvalue = register_chrdev_region(key_dev->key_cdev->dev, 1, key_dev->key_pdev->name);
+        if(retvalue != 0)
+        {
+            pr_err("key_dev dev_t register failed! \n");
+            goto freecdev;
+        }
+    #else
+        retvalue = alloc_chrdev_region(&(key_dev->key_cdev->dev), 0, 1, key_dev->key_pdev->name);
+        if(retvalue != 0)
+        {
+            pr_err("key_dev dev_t register failed! \n");
+            goto freecdev;
+        }
+    #endif
+    printk("key register dev_t success! major=%d,minor=%d\r\n", MAJOR(key_dev->key_cdev->dev), MINOR(key_dev->key_cdev->dev));
+    /*组成字符设备*/
+    key_dev->key_cdev->ops = &key_drv_fop;
+    retvalue = cdev_add(key_dev->key_cdev, key_dev->key_cdev->dev, 1);
+    if(retvalue != 0) 
+    {
+        pr_err("cannot register cdev driver\n");
+        goto freedevt;
+    }
+    /*生成设备节点*/
+    key_dev->dev = device_create(key_dev->cls, NULL,  key_dev->key_cdev->dev,  NULL,  "key_dev_%d", key_dev_count);
+    if(key_dev->dev == NULL)
+    {
+        pr_err("device_create failed!\n");
+        goto delcdev;
+    }
+    printk("key_dev_%d create success!\r\n", key_dev_count);
+    /* 初始化设备自旋锁*/
+    spin_lock_init(&key_dev->lock);
+    key_dev->status = true;
+    return 0;
+
+//错误处理
+delcdev:
+    cdev_del(key_dev->key_cdev);
+freedevt:
+    unregister_chrdev_region(key_dev->key_cdev->dev, 1);
+freecdev:
+    kfree(key_dev->key_cdev);
+freegpio:
+    gpio_free(key_dev->gpio);
+    return -EIO;
+}
+
+static int key_drv_deinit(struct key_dev_t *key_dev)
+{
+    cdev_del(key_dev->key_cdev);
+    unregister_chrdev_region(key_dev->key_cdev->dev, 1);
+    kfree(key_dev->key_cdev);
+    gpio_free(key_dev->gpio);
+    kfree(key_dev);
     return 0;
 }
 
@@ -130,55 +273,36 @@ static int key_drv_probe(struct platform_device *device)
 {
     int retvalue = 0;
 
-    /* 初始化自旋锁*/
-    spin_lock_init(&key_dev_0.lock);
-    key_dev_0.status = true;
-    /* 注册字符设备驱动 */
-    /* 查找设备结点 */
-    key_dev_0.np = of_find_compatible_node(NULL , NULL , "key_gpio");
-    if (key_dev_0.np == NULL)
+    struct key_dev_t *key_dev = kmalloc(sizeof(struct key_dev_t), GFP_KERNEL);//分配设备结构体空间
+    if(key_dev == NULL )
     {
-        pr_err("find device node failed\n");
+        pr_err("key_dev kmalloc failed! \n");
         return -EIO;
     }
-    printk("find device node successfully! \n");
-    /* 解析gpio属性 */
-    key_dev_0.gpio = of_get_named_gpio(key_dev_0.np, "gpios", 0);
-    if (key_dev_0.gpio <= 0)
+    printk("key_dev kmalloc successfully!\n");
+    key_dev->key_pdev = device;
+    retvalue = key_dev_init(key_dev);
+    if(retvalue != 0)
     {
-        pr_err("get gpio failed\n");
-        return -EIO;
+        goto freekey_dev;
     }
-    printk("get gpio %d successfully! \n", key_dev_0.gpio);
-    /* 申请gpio */
-    retvalue = gpio_request(key_dev_0.gpio, "key_gpio");
-    if (retvalue != 0)
-    {
-        pr_err("request gpio failed\n");
-        return -EIO;
-    }
-    printk("request gpio %d successfully! \n", key_dev_0.gpio);
-    /* 设置为输出且默认为1 GPIO_ACTIVE_LOW 关闭key*/
-    retvalue = gpio_direction_output(key_dev_0.gpio, 1);
-    if (retvalue != 0)
-    {
-        pr_err("request gpio output failed\n");
-        goto freegpio;
-    }
-    printk("request gpio %d output successfully! \n", key_dev_0.gpio);
-
-    /* 注册MISC设备 */
-    retvalue = misc_register(key_dev_0.misc);
-    if(retvalue != 0) 
-    {
-        pr_err("cannot register misc driver\n");
-        goto freegpio;
-    }
-    printk("key_misc_init() success! major=%d,minor=%d\r\n", 10, key_dev_0.misc->minor);
+    key_dev_count++;
+    printk("%s probe() success!\r\n",key_dev->key_pdev->name);
     return 0;
-freegpio:
-    gpio_free(key_dev_0.gpio);
-    return -EIO;
+
+freekey_dev:
+    kfree(key_dev);
+    return retvalue;
+}
+
+static int key_drv_remove(struct platform_device *device)
+{
+    /* 注销字符设备 */
+    struct key_dev_t *key_dev = container_of(&(device), struct key_dev_t, key_pdev);
+    key_drv_deinit(key_dev);
+    key_dev_count--;
+    printk("%s remove() success!\r\n",key_dev->key_pdev->name);
+    return 0;
 }
 
 static int __init key_drv_init(void)
@@ -186,55 +310,32 @@ static int __init key_drv_init(void)
     int retvalue = 0;
 
     /* 注册platform驱动 */
-    retvalue = platform_driver_register(key_dev_0.p_driver);
+    retvalue = platform_driver_register(&key_platform_driver);
     if(retvalue != 0)
     {
-        pr_err("platform driver register failed!\n");
+        pr_err("key platform driver register failed!\n");
         return -EIO;
     }
-    printk("platform driver register success!\n");
+    printk("key platform driver register success!\n");
+    key_cls = class_create(THIS_MODULE, "key_drv");
+    if(key_cls == NULL)
+    {
+        pr_err("create key_drv class failed! \n");
+        goto pdrvunrigester;
+    }
     return 0;
+pdrvunrigester:
+    platform_driver_unregister(&key_platform_driver);
+    return -EIO;
 }
 
 static void __exit key_drv_exit(void)
 {
     /* 注销platform驱动 */
-    platform_driver_unregister(key_dev_0.p_driver);
-    printk("platform driver unregister success!\n");
+    platform_driver_unregister(&key_platform_driver);
+    class_destroy(key_cls);
+    printk("key platform driver unregister success!\n");
 }
-
-static int key_drv_remove(struct platform_device *device)
-{
-    /* 注销字符设备驱动 */
-    misc_deregister(key_dev_0.misc);
-    gpio_free(key_dev_0.gpio);
-    printk("key_misc deinit\n");
-    printk("key_misc_remove() success!\r\n");
-    return 0;
-}
-
-static struct file_operations key_drv_fop = {
-    .owner = THIS_MODULE,
-    .open = key_drv_open,
-    .release = key_drv_release,
-    .write = key_drv_write,
-    .read = key_drv_read,
-};
-
-const struct of_device_id keys_of_match_table[] = {
-    {.compatible = "key_gpio",},
-    {},
-};
-
-struct platform_driver key_platform_driver = {
-    .probe = key_drv_probe,
-    .remove = key_drv_remove,
-
-    .driver = {
-        .name = "key_gpio",
-        .of_match_table = keys_of_match_table,
-    },
-};
 
 
 module_init(key_drv_init);
