746124e232516dcb78f2b9b6fc529c136f94529e
noobwang233 <1213236870@qq.com>
2023-11-13 13:10:51 +0800

    update dts and app

--- Drivers/testapp.c
+++ Drivers/testapp.c
@@ -8,50 +8,47 @@
 
 int main(int argc, char *argv[])
 {
-    int fd, retvalue, i;
-    char status;
+    int fd, retvalue;
+    char *filename;
+    unsigned char cnt = 0;
+    char readbuf[1];
 
-	/* 1. 判断参数 */
-    if(argc != 3)
+    if(argc != 2)
     {
-        printf("Usage: %s <dev> <on | off>\n", argv[0]);
+        printf("Error Usage!\r\n");
         return -1;
     }
 
-	/* 2. 打开文件 */
-	fd = open(argv[1], O_RDWR);
-	if (fd == -1)
-	{
-		printf("Can't open file %s\n", argv[1]);
-		return -1;
-	}
+    filename = argv[1];
 
-	/* 3. 读取key status */
-	if (0 == strcmp(argv[2], "off"))
-	{
-		status = 1;
-		write(fd, &status, 1);
-	}
-	else if (0 == strcmp(argv[2], "on"))
-	{
-		status = 0;
-		write(fd, &status, 1);
-	}
-    else
+    /* 打开驱动文件 */
+    fd = open(filename, O_RDWR);
+    if(fd < 0)
     {
-		printf("Error cmd! Usage: %s <dev> <on | off> \n", argv[1]);
-		return -1;
+        printf("Can't open file %s\r\n", filename);
+        return -1;
     }
-//占用设备30s
-	for(i = 0; i < 30; i++)
-	{
-		sleep(5);
-		printf("delay %d s\r\n", i);
-	}
+
+    retvalue = read(fd, readbuf, 1);
+    if(retvalue < 0){
+        printf("write file %s failed!\r\n", filename);
+        return -1;
+    }
+    printf("key status: %s \r\n", (readbuf[0] == 1 ? "on":"off"));
+
+    /* 模拟占用 25S LED */
+    while(1) {
+        sleep(5);
+        cnt++;
+        printf("App running times:%d\r\n", cnt);
+        if(cnt >= 5)
+            break;
+    }
+
     /* 关闭设备 */
     retvalue = close(fd);
     if(retvalue < 0){
-        printf("Can't close file %s\r\n", argv[1]);
+        printf("Can't close file %s\r\n", filename);
         return -1;
     }
 
