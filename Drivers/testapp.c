fb22adba0ff7b874342d1ba02c98e1ab49058fb2
noobwang233 <1213236870@qq.com>
2023-11-17 16:36:10 +0800

    fix bugs

--- Drivers/testapp.c
+++ Drivers/testapp.c
@@ -1,3 +1,4 @@
+#include "linux/input-event-codes.h"
 #include "stdio.h"
 #include "unistd.h"
 #include "sys/types.h"
@@ -43,51 +44,59 @@ int main(int argc, char *argv[])
             printf("read file %s failed!\r\n", filename);
             return -1;
         }
-        printf("key %d status: %s \r\n", num ,(ev.value == 1 ? "pushed": (ev.value == 2 ? "repeat" : "released")));
-        if(ev.value == 0 || ev.value == 1)
-        {
-            sprintf(ledfilename, "/dev/led_dev_%d", num);
-            led_fd = open(ledfilename, O_RDWR | O_NONBLOCK);
-            if(led_fd < 0)
-            {
-                printf("Can't open file %s\r\n", ledfilename);
-                return -1;
-            }
-            FD_ZERO(&writefds);
-            FD_SET(led_fd, &writefds);
-            retvalue = select(led_fd + 1, NULL, &writefds, NULL, NULL);
-            switch (retvalue) 
-            {
-                case 0: /* 超时 */ 
-                /* 用户自定义超时处理 */
-                    printf("wirte %s time out!\n", ledfilename);
-                    break; 
-                case -1: /* 错误 */ 
-                /* 用户自定义错误处理 */ 
-                    printf("wirte %s error!\n", ledfilename);
-                    return -1;
-                    break; 
-                default: 
-                    if(FD_ISSET(led_fd, &writefds)) 
+        switch (ev.type) {
+            case EV_KEY:
+                if(ev.code == KEY_0 || ev.code ==  KEY_1)
+                {
+                    printf("key %d status: %s \r\n", num ,(ev.value == 1 ? "pushed": (ev.value == 2 ? "repeat" : "released")));
+                    if(ev.value == 0 || ev.value == 1)
                     {
+                        sprintf(ledfilename, "/dev/led_dev_%d", num);
+                        led_fd = open(ledfilename, O_RDWR | O_NONBLOCK);
+                        if(led_fd < 0)
+                        {
+                            printf("Can't open file %s\r\n", ledfilename);
+                            return -1;
+                        }
+                        FD_ZERO(&writefds);
+                        FD_SET(led_fd, &writefds);
+                        retvalue = select(led_fd + 1, NULL, &writefds, NULL, NULL);
+                        switch (retvalue) 
+                        {
+                            case 0: /* 超时 */ 
+                            /* 用户自定义超时处理 */
+                                printf("wirte %s time out!\n", ledfilename);
+                                break; 
+                            case -1: /* 错误 */ 
+                            /* 用户自定义错误处理 */ 
+                                printf("wirte %s error!\n", ledfilename);
+                                return -1;
+                                break; 
+                            default: 
+                                if(FD_ISSET(led_fd, &writefds)) 
+                                {
 
-                        retvalue = write(led_fd, &ev.value, 1);
+                                    retvalue = write(led_fd, &ev.value, 1);
+                                    if(retvalue < 0){
+                                        printf("write file %s failed!\r\n", ledfilename);
+                                        return -1;
+                                    }
+                                }
+                        }
+
+                        retvalue = close(led_fd);
                         if(retvalue < 0){
-                            printf("write file %s failed!\r\n", ledfilename);
+                            printf("Can't close file %s\r\n", ledfilename);
                             return -1;
                         }
                     }
-            }
-
-            retvalue = close(led_fd);
-            if(retvalue < 0){
-                printf("Can't close file %s\r\n", ledfilename);
-                return -1;
-            }
-        }
-        else 
-        {
-            printf("key %d repeat, led keep!\n", num);
+                    else {
+                        printf("key %d repeat, led keep!\n", num);
+                    }                 
+                }
+                break;
+            default:
+                break;
         }
     }
 
